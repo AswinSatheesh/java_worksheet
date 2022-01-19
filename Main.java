cb3a0dcab17811e50f3038bbf6e9d140e567ee0f
AswinSatheesh <aswinsatheesh2001@gmail.com>
2022-01-19 15:56:23 +0530

    comments added

--- Main.java
+++ Main.java
@@ -1,5 +1,12 @@
 public class Main{
     public static void main(String[] args){
         System.out.println("Here we go!");
+        //This is a single line comment
+        /* This is a 
+        multiline comment */  
+        System.out.println("Single line comment and Multi line comment added, check the code for getting more details!");
     }
 }
+
+/* Normally we use single line comment for 
+short line and multi line comment for large line */
