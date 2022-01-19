a2d5c23bd91505edc2db5c3d9a1a0f5a078ed064
AswinSatheesh <aswinsatheesh2001@gmail.com>
2022-01-19 16:40:30 +0530

    variable example prgm added

---
+++ Names.java
@@ -0,0 +1,8 @@
+public class Names {
+    public static void main(String[] args) {
+        String fname = "Aswin ";
+        String lsname = "Satheesh";
+        String fullname = fname + lsname;
+        System.out.println(fullname);
+    }
+}
--- Variables.java
+++ Variables.java
@@ -1,7 +1,10 @@
 public class Variables {
     public static void main(String[] args){
-        String name = "Aswin";
+        String name = "Aswin"; //This is how String variable is initialising 
         System.out.println(name);
+        int myNum;
+        myNum = 5;
+        System.out.println(myNum);
     }
     
 }
