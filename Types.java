45f77e7127fecd6e0a0e26480447e6372d6099f5
AswinSatheesh <aswinsatheesh2001@gmail.com>
2022-01-20 22:25:59 +0530

    data types first prgm added

---
+++ Types.java
@@ -0,0 +1,12 @@
+public class Types{
+    public static void main(String[] args){
+        int myNum = 5;
+        float myFloatNum = 6.66f;
+        char myChar = 'A';
+        String myName = "Aswin";
+        System.out.println(myNum);
+        System.out.println(myFloatNum);
+        System.out.println(myChar);
+        System.out.println(myName);
+    }
+}
