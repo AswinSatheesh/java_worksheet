ab0739b34e309a45f4b544f266c1f4252159abd1
AswinSatheesh <aswinsatheesh2001@gmail.com>
2022-01-20 22:41:48 +0530

    code updated

--- Types.java
+++ Types.java
@@ -4,9 +4,15 @@ public class Types{
         float myFloatNum = 6.66f;
         char myChar = 'A';
         String myName = "Aswin";
+        boolean isProgrammingFun = true;
+        boolean areYoulazy = false;
+        char value = 65; //ASCII value ,because we are using 'char' instead of 'int'
         System.out.println(myNum);
         System.out.println(myFloatNum);
         System.out.println(myChar);
         System.out.println(myName);
+        System.out.println(isProgrammingFun);
+        System.out.println(areYoulazy);
+        System.out.println(value);
     }
 }
