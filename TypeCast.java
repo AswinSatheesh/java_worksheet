b2ee4c8a085fd26acee872d8c49250df4fcaa385
AswinSatheesh <aswinsatheesh2001@gmail.com>
2022-01-21 15:36:40 +0530

    Typecaste prgm added

---
+++ TypeCast.java
@@ -0,0 +1,8 @@
+public class TypeCast {
+    public static void main(String[] args){
+        int myInt = 7;
+        double myDouble = myInt;
+        System.out.println(myInt);
+        System.out.println(myDouble);
+    }
+}
