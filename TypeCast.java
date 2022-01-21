0e477431a1bfc6dac83b3c37d97af7ad23524c09
AswinSatheesh <aswinsatheesh2001@gmail.com>
2022-01-21 15:45:36 +0530

    manual casting method added

--- TypeCast.java
+++ TypeCast.java
@@ -1,8 +1,22 @@
+/* Automatic casting */
+
+// public class TypeCast {
+//     public static void main(String[] args){
+//         int myInt = 7;
+//         double myDouble = myInt; // This is a automatic type casting method (int to double)
+//         System.out.println(myInt);
+//         System.out.println(myDouble);
+//     }
+// }
+
+/* Manual casting */
+
 public class TypeCast {
     public static void main(String[] args){
-        int myInt = 7;
-        double myDouble = myInt;
-        System.out.println(myInt);
+        double myDouble = 7.87d;
+        int myInt = (int) myDouble; 
         System.out.println(myDouble);
+        System.out.println(myInt);
+    
     }
 }
