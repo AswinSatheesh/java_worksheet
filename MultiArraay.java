418152484f7703fd9d3f4a860f7801f7d59584f2
AswinSatheesh <aswinsatheesh2001@gmail.com>
2022-01-23 11:47:30 +0530

    array concept and  methods samples added

--- MultiArraay.java
+++ MultiArraay.java
@@ -1,7 +1,8 @@
 public class MultiArraay {
     public static void main(String[] args){
-        int[][] multiArray = {{1,2,3,4}, {5,6,7}};
+        int[][] multiArray = {{1,2,3,4}, {5,6,7,8}};
         // System.out.println(multiArray.length);
+        // System.out.println(multiArray[0].length);
         for (int i = 0; i < multiArray.length; ++i){
             for (int j = 0; j < multiArray[i].length; ++j){
                 System.out.println(multiArray[i][j]);
