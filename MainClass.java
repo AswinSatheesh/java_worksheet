74c31aa89b86e4a61aad1fea821f107309dc94b4
AswinSatheesh <aswinsatheesh2001@gmail.com>
2022-01-28 19:49:49 +0530

    Interface concept learned

--- MainClass.java
+++ MainClass.java
@@ -3,7 +3,7 @@ import OuterClass.InnerClass;
 public class MainClass{
     public static void main(String[] args) {
         OuterClass obj1 = new OuterClass();
-        // obj1.myfun();
+        obj1.myfun();
         OuterClass.InnerClass obj2 = obj1.new InnerClass();
         obj2.fun();
     }
